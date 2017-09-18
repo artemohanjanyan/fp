e477c43df0681e97d921866649a0c4e8133d6c2f
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-09-19 01:52:13 +0300

    Solve HW1.4

--- hw1/src/Folds.hs
+++ hw1/src/Folds.hs
@@ -1,6 +1,15 @@
 module Folds
-    ( someFunc
+    ( splitOn
+    , joinWith
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+splitOn :: (Eq a, Foldable t) => a -> t a -> [[a]]
+splitOn x = foldr it [[]]
+  where
+    it y acc@(accHead:accRest)
+        | x == y    = []:acc
+        | otherwise = (y:accHead):accRest
+    it _ [] = error "impossible"
+
+joinWith :: a -> [[a]] -> [a]
+joinWith x = foldr1 (\next acc -> next ++ [x] ++ acc)
