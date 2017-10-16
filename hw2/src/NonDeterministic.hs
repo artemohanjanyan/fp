c182f3465494458d1df5be1aedb8be3332cb00d6
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-10-16 15:59:21 +0300

    Add blocks 1 and 2 of HW2

---
+++ hw2/src/NonDeterministic.hs
@@ -0,0 +1,34 @@
+module NonDeterministic where
+
+import           Control.Monad (replicateM)
+
+bin :: Int -> [[Int]]
+--bin n | n <= 0 = [[]]
+--bin n =
+--    [0, 1] >>= \x ->
+--    bin (n - 1) >>= \xs ->
+--    pure (x:xs)
+bin = flip replicateM [0, 1]
+
+combinations :: Int -> Int -> [[Int]]
+combinations n k
+    | n == k                  = [reverse [1..n]]
+    | k == 0                  = [[]]
+    | n < k || n < 0 || k < 0 = []
+    | otherwise =
+        [True, False] >>= \shouldTake ->
+        if shouldTake
+            then (n:) <$> combinations (n - 1) (k - 1)
+            else combinations (n - 1) k
+
+permutations :: [a] -> [[a]]
+permutations [] = [[]]
+permutations list =
+    splits list >>= \(first, rest) ->
+    permutations rest >>= \restPerm ->
+    pure (first:restPerm)
+  where
+   splits :: [a] -> [(a, [a])]
+   splits []     = undefined
+   splits [x]    = [(x, [])]
+   splits (x:xs) = (x, xs) : map (fmap (x:)) (splits xs)
--- hw2/src/Optionals.hs
+++ hw2/src/Optionals.hs
@@ -1,1 +1,115 @@
+{-# LANGUAGE TypeOperators #-}
 module Optionals where
+
+import           Control.Applicative ((<|>))
+import qualified Control.Category    as C
+import           Control.Monad       ((>=>))
+import           Data.Maybe          (fromMaybe, isJust)
+
+data Expr a
+    = Expr a :+: Expr a
+    | Expr a :-: Expr a
+    | Expr a :*: Expr a
+    | Expr a :/: Expr a
+    | Expr a :^: Expr a
+    | Const a
+    deriving (Show, Eq)
+infixl 6 :+:
+infixl 6 :-:
+infixl 7 :*:
+infixl 7 :/:
+infixl 8 :^:
+
+data ArithmeticError
+    = DivisionByZero
+    | NegativeExponent
+    deriving (Show, Eq)
+
+leftIf :: (a -> Bool) -> e -> a -> Either e a
+leftIf p e x
+    | p x       = Left e
+    | otherwise = pure x
+
+eval :: Integral a => Expr a -> Either ArithmeticError a
+eval (a :+: b) = (+) <$> eval a <*> eval b
+eval (a :-: b) = (-) <$> eval a <*> eval b
+eval (a :*: b) = (*) <$> eval a <*> eval b
+eval (a :/: b) = div <$> eval a <*> (eval b >>= leftIf (== 0) DivisionByZero)
+eval (a :^: b) = (^) <$> eval a <*> (eval b >>= leftIf (< 0) NegativeExponent)
+eval (Const x) = pure x
+
+
+data a ~> b
+    = Partial   (a -> Maybe b) -- a partial function
+    | Defaulted (a ~> b) b     -- a partial function with a default value
+
+partial :: (a -> Maybe b) -> a ~> b
+partial = Partial
+
+total :: (a -> b) -> a ~> b
+total f = Partial (Just . f)
+
+apply :: (a ~> b) -> a -> Maybe b
+apply (Partial f) x       = f x
+apply (Defaulted f def) x = case apply f x of
+    Just y  -> Just y
+    Nothing -> Just def
+
+applyOrElse :: (a ~> b) -> a -> b -> b
+applyOrElse f arg def = fromMaybe def (apply f arg)
+
+withDefault :: (a ~> b) -> b -> (a ~> b)
+withDefault f@(Partial _) def   = Defaulted f def
+withDefault (Defaulted f _) def = withDefault f def
+
+isDefinedAt :: (a ~> b) -> a -> Bool
+isDefinedAt f arg = isJust $ apply f arg
+
+orElse :: (a ~> b) -> (a ~> b) -> a ~> b
+orElse f g = Partial (\x -> apply f x <|> apply g x)
+
+instance C.Category (~>) where
+    id = Partial Just
+    f . g = Partial $ apply g >=> apply f
+
+{- Lemma
+ - (f >=> g) >=> h === f >=> (g >=> h)
+ -
+ - f >=> g = \x -> f x >>= g
+ -
+ - (f >=> g) >=> h
+ - =[def (>=>)]= (\x -> f x >>= g) >=> h
+ - =[def (>=>)]= \y -> (\x -> f x >>= g) y >>= h
+ - =[beta]= \y -> (f y >>= g) >>= h
+ -
+ - f >=> (g >=> h)
+ - =[def (>=>)]= f >=> (\x -> g x >>= h)
+ - =[def (>=>)]= \y -> f y >>= (\x -> g x >>= h)
+ - =[monad law]= \y -> (f y >>= g) >>= h
+ -}
+
+{- LAWS
+ - (1) h . (g . f) === (h . g) . f
+ - (2.1) id . f === f
+ - (2.2) f . id === f
+ -
+ - (1):
+ - h . (g . f)
+ - =[def (.)]= Partial (apply (g . f) >=> apply h)
+ - =[def (.)]= Partial (apply (Partial (apply f >=> apply g)) >=> apply h)
+ - =[def apply]= Partial ((apply f >=> apply g) >=> apply h)
+ - =[Lemma]= Partial (apply f >=> (apply g >=> apply h))
+ - =[def apply)]= Partial (apply f >=> (apply (Partial (apply g >=> apply h))))
+ - =[def (.)]= Partial (apply f >=> (apply (h . g)))
+ - =[def (.)]= (h . g) . f
+ -
+ - (2):
+ - id . f
+ - =[def id]= Partial Just . f
+ - =[def (.)]= Partial (apply f >=> apply (Partial Just))
+ - =[def apply]= Partial (apply f >=> Just)
+ - =[def pure]= Partial (apply f >=> pure)
+ - =[def (>=>)]= Partial (\x -> apply f x >>= pure)
+ - =[monad law]= Partial (\x -> apply f x)
+ - =[eta equality]= Partial (apply f)
+ -}
