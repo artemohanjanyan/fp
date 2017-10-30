ef751e2fc1c54062d0307d7bd6c97f7e838fb8b7
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-10-30 15:51:11 +0300

    Add some proofs

--- hw2/src/Monads/FromMonad.hs
+++ hw2/src/Monads/FromMonad.hs
@@ -12,10 +12,33 @@ import           Monads.Definitions
 
 import           Prelude            (id)
 
+{- LAWS
+    1. m >>= return    ≡ m
+    2. return a >>= f  ≡ f a
+    3. (m >>= f) >>= g ≡ m >>= (\x -> f x >>= g)
+-}
+
 instance Monad m => MonadFish m where
     returnFish = return
     f >=> g = \x -> f x >>= g
 
+    {-
+        1. f >=> returnFish                        ≡ f
+        =[def returnFish]= f >=> return
+        =[def >=>]= \x -> f x >>= return
+        =[1.]= \x -> f x
+        =[eta]= f
+    -}
+
 instance Monad m => MonadJoin m where
     returnJoin = return
     join m = m >>= id
+
+    {-
+        1. join . returnJoin                       ≡ id
+        =[def returnJoin]= join . return
+        =[def (.)]= \x -> join (return x)
+        =[def join]= \x -> return x >>= id
+        =[2.]= \x -> id x
+        =[eta]= id
+    -}
--- hw2/src/Monads/FromMonadFish.hs
+++ hw2/src/Monads/FromMonadFish.hs
@@ -12,9 +12,23 @@ import           Monads.Definitions
 
 import           Prelude            (const, id)
 
+{- LAWS
+    1. f >=> returnFish ≡ f
+    2. returnFish >=> f ≡ f
+    3. (f >=> g) >=> h  ≡ f >=> (g >=> h)
+-}
+
 instance MonadFish m => Monad m where
     return = returnFish
     m >>= f = (const m >=> f) ()
+
+    {-
+        1. m >>= return                            ≡ m
+        =[def return]= m >>= returnFish
+        =[def >>=]= (const m >=> returnFish) ()
+        =[1.]= const m ()
+        =[def const]= m
+    -}
 
 instance MonadFish m => MonadJoin m where
     returnJoin = returnFish
