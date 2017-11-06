b597b667e9538bb0995590e264c42e24fbffb1a6
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-11-06 15:58:39 +0300

    Implement boolean table calculator

---
+++ bonus/src/BooleanTable.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module BooleanTable where
+
+import           Control.Monad  (forM_)
+import           Data.Bifunctor (first)
+
+class BooleanTableDomain a where
+    makeTable :: a -> [([Bool], Bool)]
+
+instance BooleanTableDomain Bool where
+    makeTable = pure . pure
+
+instance BooleanTableDomain a => BooleanTableDomain (Bool -> a) where
+    makeTable f = do
+        arg <- [True, False]
+        restTable <- makeTable (f arg)
+        pure $ first (arg:) restTable
+
+printTable :: BooleanTableDomain a => a -> IO ()
+printTable f = do
+    let table = makeTable f
+    forM_ table printRow
+  where
+    printRow :: ([Bool], Bool) -> IO ()
+    printRow (args, ans) = do
+        forM_ args (putStr . boolToAlignedStr)
+        putStr "= "
+        putStrLn (show ans)
+
+    boolToAlignedStr :: Bool -> String
+    boolToAlignedStr True  = "True  "
+    boolToAlignedStr False = "False "
